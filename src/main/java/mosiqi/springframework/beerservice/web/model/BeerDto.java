fefa7ce9806da7c23067367496cea8e400dde0bd
mokszekei <mosiqi1996@hotmail.com>
2020-12-18 15:07:23 -0500

    Adding data model. Colses #2

---
+++ src/main/java/mosiqi/springframework/beerservice/web/model/BeerDto.java
@@ -0,0 +1,37 @@
+package mosiqi.springframework.beerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by Siqi on 2020-12-18.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+
+    private Long upc; //stock index
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/mosiqi/springframework/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package mosiqi.springframework.beerservice.web.model;
+
+/**
+ * Created by jt on 2020-12-18.
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
